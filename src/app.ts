8a8535753cc527574e9bfc9c154e64676461e75e
Erick <eopalomi@gmail.com>
2023-03-16 20:40:06 -0500

    add enviroment default

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,8 @@
 import express, { json } from 'express'
 import { authenticateUser } from './controllers/login.controller';
+import dotenv from 'dotenv';
+
+dotenv.config({path:`.env.${process.env.NODE_ENV}`});
 
 const app = express();
 
@@ -9,6 +12,6 @@ const host = process.env.HOST || '0.0.0.0';
 app.use(json());
 app.post('/login', authenticateUser);
 
-app.listen(port, ()=>{
+app.listen({port: port, host: host}, () => {
     console.log("Servidor corriendo en el puerto " + port)
 });
